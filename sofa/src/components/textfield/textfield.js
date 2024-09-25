f1aaf84b63020508d9525947c160d601426a7411
Seong-Myeong <mg0454@naver.com>
2024-09-25 15:16:37 +0900

    Feat: 텍스트필드 추가 (#1)
    텍스트필드 js, css 기본틀 제작

---
+++ sofa/src/components/textfield/textfield.js
@@ -0,0 +1,21 @@
+// src/components/TextField.js
+import React from 'react';
+import './textfield.css'; // 스타일 파일 (선택)
+
+const TextField = ({ label, value, onChange, placeholder, type = 'text', required = false }) => {
+  return (
+    <div className="text-field">
+      {label && <label className="text-field-label">{label}</label>}
+      <input
+        className="text-field-input"
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export default TextField;
