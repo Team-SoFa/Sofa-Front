138c02a4322db18b9c38ec6c6c355ea555c661d4
Bangdayeon <dybang00@gmail.com>
2024-09-27 07:49:33 -0400

    Feat: 북마크 기본형태 제작

--- sofa/src/App.js
+++ sofa/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BookmarkForm from "./components/BookmarkForm";
+import BookmarkList from "./components/BookmarkList";
+import "./App.css";
 
 function App() {
+  // 북마크 상태 관리
+  const [bookmarks, setBookmarks] = useState([
+    { id: 1, title: "Google", url: "htttps://google.com" },
+    { id: 2, title: "Github", url: "https://github.com" },
+  ]);
+
+  // 북마크 추가 함수
+  const addBookmark = (newBookmark) => {
+    // 새로운 북마크에 고유 ID 추가
+    const newId = bookmarks.length ? bookmarks[bookmarks.length - 1].id + 1 : 1;
+    const bookmarkWithId = { id: newId, ...newBookmark };
+    // 기존 북마크에 추가
+    setBookmarks([...bookmarks, bookmarkWithId]);
+  };
+
+  // 북마크 삭제 함수
+  const deleteBookmark = (id) => {
+    const filteredBookmarks = bookmarks.filter(
+      (bookmark) => bookmark.id !== id
+    );
+    setBookmarks(filteredBookmarks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bookmark</h1>
+      <BookmarkForm onSubmit={addBookmark} />
+      <BookmarkList bookmarks={bookmarks} onDelete={deleteBookmark} />
     </div>
   );
 }
---
+++ sofa/src/components/BookmarkItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//boomark : 북마크 데이터(id, title, url) 저장하는 객체
+//onDelete : 북마크의 id를 인자로 받아 삭제처리하는 콜백 함수
+
+//target="_black" : 새 탭에서 링크 오픈
+//noopener noreferrer : 외부 링크를 새 탭에서 열 때 참조 링크 정보 전달x(보안상 이유)
+//bookmark.title : 북마크 제목을 화면에 표시(클릭하면 해당 URL로 이동)
+const BookmarkItem = ({ bookmark, onDelete }) => {
+  return (
+    <div className="bookmark-item">
+      <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
+        {bookmark.title}
+      </a>
+      <button onClick={() => onDelete(bookmark.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default BookmarkItem;
