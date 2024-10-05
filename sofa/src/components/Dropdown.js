9ca28fb7276a5e45eeccd1bf141feaef521349e1
Bangdayeon <dybang00@gmail.com>
2024-10-04 22:58:31 -0400

    드롭다운 js파일 작성

---
+++ sofa/src/components/Dropdown.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "../styles/Dropdown.css";
+
+const Dropdown = () => {
+  const [selectedValue, setSelectedValue] = useState(""); //선택된 값 상태 관리
+  const options = ["Option1", "Option2", "Option3"]; //드롭박스 옵션 리스트
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <select value={selectedValue} onChange={handleChange}>
+        <option value="" disabled>
+          옵션을 선택하세용
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      {selectedValue && <p>당신이 고른 것 : {selectedValue}</p>}
+    </div>
+  );
+};
+
+export default Dropdown;
