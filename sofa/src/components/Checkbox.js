37d01df320356aff4514a2cdd1eec7e5c3314b92
Bangdayeon <dybang00@gmail.com>
2024-10-05 00:21:23 -0400

    Feat: 단일 체크박스

---
+++ sofa/src/components/Checkbox.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Checkbox = () => {
+  const [checked, setChecked] = useState(false); // 체크박스 상태
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        {checked ? "Checkec" : "Unchecked"}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
