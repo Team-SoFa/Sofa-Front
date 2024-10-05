327800019c00191661d10c4126eb6c2e3c5b16ad
Bangdayeon <dybang00@gmail.com>
2024-10-05 02:06:00 -0400

    Design: 스피너 디자인 코드 작성

--- sofa/src/App.js
+++ sofa/src/App.js
@@ -1,16 +1,25 @@
-import "./App.css";
-import Checkbox from "./components/Checkbox";
-import Dropdown from "./components/Dropdown";
-import MultiCheckbox from "./components/MultiCheckbox";
+import React, { useState, useEffect } from "react";
+import Spinner from "./components/Spinner"; // 스피너 컴포넌트 import
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // 3초 후 로딩 종료
+    }, 7000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Dropdown Example</h1>
-      <Dropdown />
-      <Checkbox />
+    <div>
+      {loading ? (
+        <Spinner /> // 로딩 중이면 스피너 보여줌
+      ) : (
+        <h1>로딩 완료!</h1> // 로딩이 끝나면 다른 내용 보여줌
+      )}
     </div>
   );
-}
+};
 
 export default App;
