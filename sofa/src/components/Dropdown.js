2d482499ad8a8684105b7ec93173686cd9148e13
Bangdayeon <dybang00@gmail.com>
2024-10-05 01:43:31 -0400

    Design: dropdown 스타일 적용

--- sofa/src/components/Dropdown.js
+++ sofa/src/components/Dropdown.js
@@ -3,7 +3,7 @@ import "../styles/Dropdown.css";
 
 const Dropdown = () => {
   const [selectedValue, setSelectedValue] = useState(""); //선택된 값 상태 관리
-  const options = ["Option1", "Option2", "Option3"]; //드롭박스 옵션 리스트
+  const options = ["쿵야", "호놀룰루", "이손을봐.대박임."]; //드롭박스 옵션 리스트
 
   const handleChange = (e) => {
     setSelectedValue(e.target.value);
@@ -17,7 +17,7 @@ const Dropdown = () => {
         onChange={handleChange}
       >
         <option value="" disabled>
-          옵션을 선택하세용
+          폴더선택
         </option>
         {options.map((option, index) => (
           <option key={index} value={option}>
@@ -25,9 +25,6 @@ const Dropdown = () => {
           </option>
         ))}
       </select>
-      {selectedValue && (
-        <p className="selected-value">당신이 고른 것 : {selectedValue}</p>
-      )}
     </div>
   );
 };
