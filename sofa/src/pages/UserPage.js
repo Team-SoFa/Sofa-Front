ec84ef7636966df740623c5f48db73afcb10ef58
Bangdayeon <dybang00@gmail.com>
2024-10-11 10:25:37 -0400

    Feat:grid bookmark 리스트 제외 컴포넌트 추가

--- sofa/src/pages/UserPage.js
+++ sofa/src/pages/UserPage.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import Header from "../components/Layout/Header";
+import TextField from "../components/textfield/textfield";
+import Dropdown from "../components/Dropdown/Dropdown";
+import BookmarkList from "../components/Bookmark/BookmarkList";
 
 const UserPage = () => {
   return (
     <div className="userpage">
       <Header />
-      <h1>UserPage입니다</h1>
+      <TextField />
+      <h2>어쩌고폴더</h2>
+      <Dropdown />
+      <Dropdown />
     </div>
   );
 };
