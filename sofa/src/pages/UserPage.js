9cc4155f904322e65423080f5622a1544a099cc6
Bangdayeon <dybang00@gmail.com>
2024-10-11 09:37:56 -0400

    Feat: 유저 페이지 생성 및 App.js에 Route 추가

---
+++ sofa/src/pages/UserPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const UserPage = () => {
+  return (
+    <div className="userpage">
+      <h1>UserPage입니다</h1>
+    </div>
+  );
+};
+
+export default UserPage;
