4714bd5bf36c7b42ed45b4de6620b2848c838eee
Bangdayeon <dybang00@gmail.com>
2024-10-11 10:12:22 -0400

    Design: 유저페이지와 다른 페이지 헤더 디자인 통합

--- sofa/src/components/Layout/Header.js
+++ sofa/src/components/Layout/Header.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./header-layout.css";
 
 const Header = () => {
+  const location = useLocation();
   return (
     <header className="header">
-      <img
-        className="logo"
-        src="example.png"
-        alt="logo"
-        onClick={() => window.location.reload()}
-      />
-      <div className="buttons">
-        <button className="header-btn">확장 프로그램 추가하기</button>
-        <Link to="/login">
-          <button className="header-btn">시작하기</button>
-        </Link>
-      </div>
+      {location.pathname === "/userpage" && (
+        <>
+          <Link to="/userpage">
+            <img
+              className="menu"
+              src="menu-icon.png"
+              width={"25px"}
+              alt="menu"
+              onClick={() => window.location.reload()}
+            />
+          </Link>
+        </>
+      )}
+      {location.pathname !== "/userpage" && (
+        <>
+          <img
+            className="logo"
+            src="example.png"
+            alt="logo"
+            onClick={() => window.location.reload()}
+          />
+          <div className="buttons">
+            <button className="header-btn">확장 프로그램 추가하기</button>
+            <Link to="/login">
+              <button className="header-btn">시작하기</button>
+            </Link>
+            <Link to="/userpage">
+              <button className="header-btn">[임시]유저페이지</button>
+            </Link>
+          </div>
+        </>
+      )}
     </header>
   );
 };
--- sofa/src/pages/UserPage.js
+++ sofa/src/pages/UserPage.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import Header from "../components/Layout/Header";
 
 const UserPage = () => {
   return (
     <div className="userpage">
+      <Header />
       <h1>UserPage입니다</h1>
     </div>
   );
