9cff530e4ffbfe9c0c436fc1e9fce362600782c1
Bangdayeon <dybang00@gmail.com>
2024-10-12 01:01:14 -0400

    Docs: 유저별페이지 테스트를 용이하게 하기 위해 북마크 리스트 추가

--- sofa/src/pages/UserPage.js
+++ sofa/src/pages/UserPage.js
@@ -5,7 +5,54 @@ import BookmarkList from "../components/Bookmark/BookmarkList";
 import BookmarkForm from "../components/Bookmark/BookmarkForm";
 
 const UserPage = () => {
-  const [bookmarks, setBookmarks] = useState([]);
+  const TestBookmarks = [
+    {
+      title: "Google",
+      url: "https://www.google.com",
+    },
+    {
+      title: "(NAVER)URL은 짧은데 제목이 길어질 경우",
+      url: "https://www.naver.com",
+    },
+    {
+      title:
+        "GitHub 이정도로 길어지지 않게 제한 두는 게 좋을 것 같지만 naver보다 더 길어진다면",
+      url: "https://www.github.com",
+    },
+    {
+      title: "Stack Overflow",
+      url: "https://stackoverflow.com",
+    },
+    {
+      title: "MDN Web Docs",
+      url: "https://developer.mozilla.org",
+    },
+    { title: "React", url: "https://reactjs.org" },
+    {
+      title: "YouTube",
+      url: "https://www.youtube.com",
+    },
+    {
+      title: "스택 프레임 이해하기",
+      url: "https://velog.io/@hamham/%EC%8A%A4%ED%83%9D%ED%94%84%EB%A0%88%EC%9E%84-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0",
+    },
+    {
+      title: "URL이 두줄인데 제목도 두줄까지 길어진다면",
+      url: "https://www.kw.ac.kr/ko/life/notice.jsp?BoardMode=view&DUID=47837&tpage=1&searchKey=1&searchVal=&srCategoryId=",
+    },
+    { title: "제목이랍니다", url: "https://www.youtube.com" },
+    { title: "키스오브라이프", url: "https://m.cafe.daum.net/KISSOFLIFE/_rec" },
+    {
+      title: "나의 ex..컬러버스<3",
+      url: "https://www.mk.co.kr/news/it/10882354",
+    },
+    {
+      title: "고슴도치의 가시는 털일까?",
+      url: "https://m.blog.naver.com/dnjsifjqjd/221181323254",
+    },
+  ];
+
+  const [bookmarks, setBookmarks] = useState(TestBookmarks);
   const handleAddBookmark = (newBookmark) => {
     setBookmarks([...bookmarks, { ...newBookmark, id: Date.now() }]);
   };
