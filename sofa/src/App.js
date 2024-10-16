83885c46ecc37b0e0414ab62fda053be7f87a18d
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2024-10-17 05:36:44 +0900

    Feat: onBorarding and sign Pages (#5)
    페이지 추가
    - onBoarding Page
    - sign Page

--- sofa/src/App.js
+++ sofa/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom"; // 라우터 import
-import OnBoadringPage from "./pages/OnBoardingPage";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<OnBoadringPage />} />
+      {/* <Route path="/" element={<OnBoardingPage />} /> */}
+      <Route path="/" element={<SignInPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
