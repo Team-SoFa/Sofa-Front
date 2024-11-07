c56f5417154f1f6ec5b42dfeb116ddf3559985e2
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2024-11-07 22:48:33 +0900

    Feat: Modal Component 추가

--- sofa/src/components/Layout/Header.js
+++ sofa/src/components/Layout/Header.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./header-layout.css";
+import Modal from "../Modal/Modal"; // Modal 컴포넌트 import
 
 const Header = () => {
   const location = useLocation();
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 추가
+
+  // 모달 열기 함수
+  const openModal = () => setIsModalOpen(true);
+
+  // 모달 닫기 함수
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <header className="header">
       {location.pathname === "/userpage" && (
@@ -38,9 +47,21 @@ const Header = () => {
             <Link to="/signpage">
               <button className="header-btn">[임시]SignPage</button>
             </Link>
+            {/* 모달 열기 버튼 */}
+            <button className="header-btn" onClick={openModal}>
+              [임시]Modal
+            </button>
           </div>
         </>
       )}
+
+      {/* Modal 컴포넌트 */}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2 style={{ marginBottom: '1rem '}}>Example for Modal</h2>
+        <p>모달 내용입니다.</p>
+        <p>모</p>
+        <p>달</p>
+      </Modal>
     </header>
   );
 };
