7d181cb705f261a455ee6bf4467b08d8337e79e2
Seong-Myeong <57748493+Seong-Myeong@users.noreply.github.com>
2024-11-07 18:15:44 +0900

    Feat: png 확장자 맞춤

--- sofa_extension/background.js
+++ sofa_extension/background.js
@@ -15,18 +15,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         // 변경할 아이콘
         chrome.action.setIcon({
           path: {
-            "16": "assets/logo216.png",
-            "48": "assets/logo248.png",
-            "128": "assets/logo2128.png"
+            "16": "assets/16/logo_png2_16.png",
+            "48": "assets/48/logo_png2_48.png",
+            "128": "assets/128/logo_png2_128.png"
           }
         });
       } else {
         // 기본 아이콘
         chrome.action.setIcon({
           path: {
-            "16": "assets/logo16.png",
-            "48": "assets/logo48.png",
-            "128": "assets/logo128.png"
+            "16": "assets/16/logo_png1_16.png",
+            "48": "assets/48/logo_png1_48.png",
+            "128": "assets/128/logo_png1_128.png"
           }
         });
       }
