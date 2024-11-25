0f38738472db855a826685c7fe384f7b100583e1
Bangdayeon <dybang00@gmail.com>
2024-11-25 17:16:02 +0900

    Feature: 컴포넌트 외부 클릭 시 닫힘 기능

--- sofa/src/components/Dropdown/Dropdown.js
+++ sofa/src/components/Dropdown/Dropdown.js
@@ -1,29 +1,24 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Dropdown.css";
+import { OutsideClick } from "../OutsideClick";
 
 const Dropdown = ({ className, options, label }) => {
+  const dropdownRef = useRef(null); //드롭다운 요소 참조를 위한 ref 생성
+  const [isOpen, setIsOpen] = OutsideClick(dropdownRef, false); //OutsideClick 사용
   const [selectedValue, setSelectedValue] = useState(null); //선택된 값 상태 관리
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef(null);
-
-  useEffect(() => {
-    const handleOutsideClick = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsOpen(false); //close dropdown
-      }
-    };
-  });
 
   const handleSelect = (value) => {
+    //옵션 선택 시, 선택된 값 저장
     setSelectedValue(value);
     setIsOpen(false);
   };
   const toggleDropdown = () => {
+    //드롭다운 토글 함수
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`dropdown ${className}`}>
+    <div className={`dropdown ${className}`} ref={dropdownRef}>
       <div className="dropdown-header" onClick={toggleDropdown}>
         {selectedValue ? (
           selectedValue.label
