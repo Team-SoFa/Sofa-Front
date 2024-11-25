cce807f7dc962f3a4709bd33549cf794288e9508
Bangdayeon <dybang00@gmail.com>
2024-11-25 17:01:13 +0900

    Modify: 드롭다운 옵션 내용 출력

--- sofa/src/components/Dropdown/Dropdown.js
+++ sofa/src/components/Dropdown/Dropdown.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Dropdown.css";
 
 const Dropdown = ({ className, options, label }) => {
   const [selectedValue, setSelectedValue] = useState(null); //선택된 값 상태 관리
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false); //close dropdown
+      }
+    };
+  });
 
   const handleSelect = (value) => {
     setSelectedValue(value);
