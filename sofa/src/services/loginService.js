d8c0600a56f5cacc81eb8306f271046d093a41f5
Seong-Myeong <mg0454@naver.com>
2024-12-02 19:19:06 +0900

    Feat: ENV

--- sofa/src/services/loginService.js
+++ sofa/src/services/loginService.js
@@ -4,7 +4,7 @@ import apiClient from "./apiClient"; // apiClient를 올바르게 가져옴
 export const getGoogleLoginUrl = async () => {
   try {
     console.log("Base URL:", apiClient.defaults.baseURL);
-    const response = await axios.get("/login/oauth2/google");
+    const response = await axios.get("/oauth2/authorization/google");
     return response.data;
   } catch (error) {
     console.error("Failed to fetch Google login URL:", error);
