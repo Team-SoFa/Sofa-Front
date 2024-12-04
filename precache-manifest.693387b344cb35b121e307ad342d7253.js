ad0a7f0a1cd13723f55639bc3b2ce2e1052adcf6
Seong-Myeong <mg0454@naver.com>
2024-12-05 06:19:01 +0900

    Updates

---
+++ precache-manifest.693387b344cb35b121e307ad342d7253.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db32dc094d95ca19509e748500c45063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2582ca185aba42dd2ec4",
+    "url": "/static/css/main.01587f65.chunk.css"
+  },
+  {
+    "revision": "00bd645ce4d5c3a44866",
+    "url": "/static/js/2.75631837.chunk.js"
+  },
+  {
+    "revision": "0d3236f0da39143fd5a0e10aed39db9e",
+    "url": "/static/js/2.75631837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2582ca185aba42dd2ec4",
+    "url": "/static/js/main.aab4272d.chunk.js"
+  },
+  {
+    "revision": "b0b4c202eed4b06a6339",
+    "url": "/static/js/runtime-main.167eaf22.js"
+  },
+  {
+    "revision": "40b92fb6e0e447ca7f0c7fddb6cec9c6",
+    "url": "/static/media/Pretendard-Bold.subset.40b92fb6.woff2"
+  },
+  {
+    "revision": "6dec4607c4804775486c0b22e1b96f4b",
+    "url": "/static/media/Pretendard-ExtraBold.subset.6dec4607.woff2"
+  },
+  {
+    "revision": "e55a007a88db0a7a45bbeedd0bc9ce77",
+    "url": "/static/media/Pretendard-Medium.subset.e55a007a.woff2"
+  },
+  {
+    "revision": "97213db5c578498e3bab52fb858e720a",
+    "url": "/static/media/Pretendard-Regular.subset.97213db5.woff2"
+  },
+  {
+    "revision": "3ba9cd1e8bcb4824f073865d2ed81d34",
+    "url": "/static/media/Pretendard-SemiBold.subset.3ba9cd1e.woff2"
+  }
+]);
