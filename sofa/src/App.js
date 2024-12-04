2b91b810cda51b5663ae29d4c717e7ead16bf543
Bangdayeon <dybang00@gmail.com>
2024-12-04 13:15:02 +0900

    etc: app/textfield 대소문자 수정

--- sofa/src/App.js
+++ sofa/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom"; // 라우터 import
 import LandingPage from "./pages/LandingPage";
 import SignPage from "./pages/SignPage";
