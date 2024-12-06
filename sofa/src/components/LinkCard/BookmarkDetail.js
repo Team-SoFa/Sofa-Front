6e679cf6da3755cce03369a7b1593ce5cb853f85
Bangdayeon <dybang00@gmail.com>
2024-12-07 01:40:52 +0900

    Feat:텍스트 편집 함수 작성 및 변수/함수 정리

--- sofa/src/components/LinkCard/BookmarkDetail.js
+++ sofa/src/components/LinkCard/BookmarkDetail.js
@@ -19,24 +19,30 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
     if (!bookmark || !Array.isArray(bookmarks)) {
       return -1; // 초기 값으로 -1 설정
     }
-
     return bookmarks.findIndex((item) => item?.id === bookmark?.id);
   });
-  const [isReminderActive, setIsReminderActive] = useState(false);
-  const [linkcardImg, setLinkcardImg] = useState(""); //링크카드 대표이미지 변수
-  // title
-  const originalTitleRef = useRef("");
+  //
   const titleRef = useRef(null);
-  const [isEditingTitle, setIsEditingTitle] = useState(false);
-  const [title, setTitle] = useState(bookmark?.title || ""); //제목 관리
-  // summary
-  const [summary, setSummary] = useState("요약 내용입니다");
-  const [isEditingSummary, setIsEditingSummary] = useState(false);
-  // memo
-  const [memo, setMemo] = useState("메모 내용입니다");
-  const [isEditingMemo, setIsEditingMemo] = useState(false);
+  const [values, setValues] = useState({
+    title: bookmark?.title || "",
+    summary: bookmark?.summary || "요약 내용입니다",
+    memo: bookmark?.memo || "메모 내용입니다",
+  });
+  const originalValuesRef = useRef({
+    title: "",
+    summary: "",
+    memo: "",
+  });
+  const [isEditing, setIsEditing] = useState({
+    title: false,
+    summary: false,
+    memo: false,
+  });
 
-  // 예시 데이터 추가
+  const [linkcardImg, setLinkcardImg] = useState(""); //링크카드 대표이미지 변수
+
+  // 임시 데이터 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  const [isReminderActive, setIsReminderActive] = useState(false);
   const [tagsOpt, setTagsOpt] = useState([
     { label: "Documents", content: "Documents" },
     { label: "Pictures", content: "Pictures" },
@@ -47,9 +53,6 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
       label: "모던웹을위한Javascript",
       content: "모던웹을위한Javascript+jQuery입문",
     },
-    { label: "코스모스", content: "코스모스" },
-    { label: "소년이온다", content: "소년이온다" },
-    { label: "이것이자바다", content: "이것이자바다" },
   ]);
   const folderOpt = ["폴더1", "오렌지방구는누가꼈나", "어쩌구", "우와우"].map(
     (item) => ({
@@ -58,21 +61,35 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
     })
   );
 
-  // 북마크 업데이트 될 때마다의 동작
-  //Id Update
+  // Activates Updating Bookmark
   useEffect(() => {
     if (bookmarks.length > 0 && bookmark) {
+      // Update ID
       const index = bookmarks.findIndex((item) => item.id === bookmark.id);
       setCurrentIndex(index);
+      if (bookmark.img) {
+        // Update Image
+        setLinkcardImg(bookmark.img);
+      } else {
+        setLinkcardImg("example.png");
+      }
+      // Update Title, Summary, and Memo
+      setValues({
+        title: bookmark.title || "",
+        summary: bookmark.summary || "요약 내용입니다.",
+        memo: bookmark.memo || "메모 내용입니다.",
+      });
+
+      // Update editing state to false when bookmark is loaded
+      setIsEditing({
+        title: false,
+        summary: false,
+        memo: false,
+      });
     }
   }, [bookmarks, bookmark]);
-  // Title Update
-  useEffect(() => {
-    if (bookmark?.title) {
-      setTitle(bookmark.title);
-    }
-  }, [bookmark]);
 
+  // HEADER BTNS >>>>>>>>>>>>>>>>>>>>>>>>>>>>
   const icons = [
     { id: "left", Icon: LeftIcon },
     { id: "right", Icon: RightIcon },
@@ -80,7 +97,6 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
     { id: "trash", Icon: TrashIcon },
     { id: "close", Icon: CloseIcon },
   ];
-
   const handleIconClick = (id) => {
     switch (id) {
       case "left":
@@ -88,11 +104,14 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
           const prevBookmark = bookmarks[currentIndex - 1];
           setCurrentIndex(currentIndex - 1);
           // 북마크 정보 업데이트
-          setTitle(prevBookmark.title);
-          setSummary(prevBookmark.summary);
-          setMemo(prevBookmark.memo);
+          setValues((prevValues) => ({
+            ...prevValues,
+            title: prevBookmark.title,
+            summary: prevBookmark.summary,
+            memo: prevBookmark.memo,
+          }));
         } else {
-          console.log("이전 북마크가 없습니다");
+          //❗알림 추가❗
         }
         break;
       case "right":
@@ -100,23 +119,23 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
           const nextBookmark = bookmarks[currentIndex + 1];
           setCurrentIndex(currentIndex + 1);
           // 북마크 정보 업데이트
-          setTitle(nextBookmark.title);
-          setSummary(nextBookmark.summary);
-          setMemo(nextBookmark.memo);
+          setValues((nextValues) => ({
+            ...nextValues,
+            title: nextBookmark.title,
+            summary: nextBookmark.summary,
+            memo: nextBookmark.memo,
+          }));
         } else {
-          console.log("다음 북마크가 없습니다");
+          //❗알림 추가❗
         }
         break;
       case "remind":
         setIsReminderActive(!isReminderActive);
-        // 여기에 토스트 알림 로직 추가
-        console.log("Reminder toggled!");
         break;
       case "trash":
         if (currentIndex < bookmarks.length - 1) {
           const nextBookmark = bookmarks[currentIndex + 1];
-          // 북마크 삭제 로직 추가 (북마크 리스트 업데이트 필요)
-          console.log("Bookmark deleted:", bookmark.id);
+          // ❗북마크 삭제 로직 추가❗
           setCurrentIndex(currentIndex + 1);
         } else if (currentIndex > 0) {
           const prevBookmark = bookmarks[currentIndex - 1];
@@ -131,20 +150,15 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
         break;
     }
   };
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<< HEADER BTNS
 
-  const handleLinkcardImg = () => {
-    setLinkcardImg("example.png");
-  };
-  useEffect(() => {
-    handleLinkcardImg(); // bookmark가 변경될 때마다 이미지 설정
-  }, [bookmark]);
-
+  // [ TAGS ]
   const handleTagDelete = (tagToDelete) => {
     const updatedTags = tagsOpt.filter((tag) => tag.content !== tagToDelete); // content 기준으로 삭제
     setTagsOpt(updatedTags); // 상태 업데이트
   };
 
-  // calc summary, memo container height
+  // Summary, Memo 높이 동적 계산
   const useDynamicHeight = (content) => {
     const containerRef = useRef(null);
     const [containerHeight, setContainerHeight] = useState("auto");
@@ -159,47 +173,59 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
 
     return { containerRef, containerHeight };
   };
-  // Summary와 Memo 각각 훅 호출
+  // 높이 저장용 SummaryRef, MemoRef
   const { containerRef: summaryRef, containerHeight: summaryHeight } =
-    useDynamicHeight(summary);
+    useDynamicHeight(values.summary);
   const { containerRef: memoRef, containerHeight: memoHeight } =
-    useDynamicHeight(memo);
+    useDynamicHeight(values.memo);
 
-  //수정 모드 종료(저장)
-  const saveTitle = () => {
-    setIsEditingTitle(false);
-  };
-  const saveSummary = () => {
-    setIsEditingSummary(false);
-  };
-  const saveMemo = () => {
-    setIsEditingMemo(false);
-  };
-
-  // 제목/수정 클릭 시 원래 제목 저장
-  const handleEditClick = () => {
-    originalTitleRef.current = title; // 수정 전 제목을 ref에 저장
-    setIsEditingTitle(true); // 수정 모드로 전환
+  // 텍스트 수정 관련 >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //"수정"
+  const handleEdit = (field) => {
+    originalValuesRef.current[field] = values[field]; // 수정 전 값을 저장
+    setIsEditing((prev) => ({ ...prev, [field]: true })); // 해당 필드만 수정 모드로 전환
+    // focus 설정 (각각의 ref에 대해 focus 처리)
     setTimeout(() => {
-      if (titleRef.current) {
+      if (field === "title" && titleRef.current) {
         titleRef.current.focus();
         titleRef.current.setSelectionRange(
           titleRef.current.value.length,
           titleRef.current.value.length
         );
+      } else if (field === "summary" && summaryRef.current) {
+        summaryRef.current.focus();
+        summaryRef.current.setSelectionRange(
+          summaryRef.current.value.length,
+          summaryRef.current.value.length
+        );
+      } else if (field === "memo" && memoRef.current) {
+        memoRef.current.focus();
+        memoRef.current.setSelectionRange(
+          memoRef.current.value.length,
+          memoRef.current.value.length
+        );
       }
     }, 0);
   };
 
-  // 제목/취소 버튼 클릭 시 수정 전 제목으로 되돌리기
-  const handleCancelClick = () => {
-    setIsEditingTitle(false); // 수정 모드 종료
-    setTitle(originalTitleRef.current); // 수정 전 제목으로 되돌리기
+  // "완료"
+  const handleSave = (field) => {
+    setIsEditing((prev) => ({ ...prev, [field]: false })); // 수정 모드 종료
   };
-
-  // 제목/완료 버튼 클릭 시 제목 저장
-  const handleSaveClick = () => {
-    setIsEditingTitle(false); // 수정 모드 종료
+  // "취소"
+  const handleCancel = (field) => {
+    setIsEditing((prev) => ({ ...prev, [field]: false })); // 수정 모드 종료
+    setValues((prev) => ({
+      ...prev,
+      [field]: originalValuesRef.current[field],
+    }));
+  };
+  // 각 필드에 대한 value 업데이트
+  const handleChange = (e, field) => {
+    setValues((prev) => ({
+      ...prev,
+      [field]: e.target.value,
+    }));
   };
 
   if (!bookmark) return null;
@@ -235,151 +261,154 @@ const BookmarkDetail = ({ bookmark, bookmarks, isOpen, toggleDetail }) => {
               label="폴더선택"
             />
           </div>
+
           <div className="detail-title-container">
-            {isEditingTitle ? (
+            {isEditing.title ? (
               <textarea
                 ref={titleRef}
                 className="detail-text-input"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                value={values.title}
+                onChange={(e) => handleChange(e, "title")}
                 onInput={(e) => {
                   const target = e.target;
                   target.style.height = "4rem"; // 높이를 초기화
                   target.style.height = `${target.scrollHeight}px`; // 내용 기반으로 높이 조정
                 }}
-                onBlur={saveTitle} // 입력 필드에서 포커스 해제 시 저장
+                onBlur={() => {
+                  handleCancel("title");
+                }}
                 onKeyDown={(e) => {
                   if (e.key === "Enter") {
                     e.preventDefault(); // 기본 동작 방지
-                    saveTitle(); // 저장 처리
+                    handleSave("title");
                   }
                 }}
               />
             ) : (
-              <span className="detail-title">{title}</span>
+              <span className="detail-title">{values.title}</span>
             )}
           </div>
-          {isEditingTitle ? (
+          {isEditing.title ? (
             <div className="detail-edit-buttons">
               <Button
                 className="detail-cancel"
                 label="취소"
-                onClick={handleCancelClick} // 취소 버튼 클릭 시 원래 제목으로 되돌리기
+                onClick={() => handleCancel("title")}
               />
               <Button
                 className="detail-save"
                 label="완료"
-                onClick={handleSaveClick} // 완료 버튼 클릭 시 저장 처리
+                onClick={() => handleSave("title")}
               />
             </div>
           ) : (
             <Button
               className="detail-edit"
               label="수정"
-              onClick={handleEditClick} // 수정 버튼 클릭 시 수정 모드로 전환
+              onClick={() => handleEdit("title")}
             />
           )}
         </div>
       </div>
 
-      {/* 자동 요약 */}
       <div className="summary-container" style={{ height: summaryHeight }}>
         <p>자동 요약</p>
         <div className="detail-text-container">
-          {isEditingSummary ? (
+          {isEditing.summary ? (
             <textarea
               ref={summaryRef}
               className="detail-text-input"
-              value={summary}
-              onChange={(e) => setSummary(e.target.value)}
+              value={values.summary}
+              onChange={(e) => handleChange(e, "summary")}
               onInput={(e) => {
                 const target = e.target;
                 target.style.height = "6rem"; // 높이를 초기화
                 target.style.height = `${target.scrollHeight}px`; // 내용 기반으로 높이 조정
               }}
-              onBlur={saveSummary} // 입력 필드에서 포커스 해제 시 저장
+              onBlur={() => {
+                handleCancel("summary");
+              }} // 입력 필드에서 포커스 해제 시 저장
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   e.preventDefault(); // 기본 동작 방지
-                  saveSummary();
+                  handleSave("summary");
                 }
               }}
             />
           ) : (
-            <p className="detail-text">{summary}</p>
+            <p className="detail-text">{values.summary}</p>
           )}
         </div>
-        <Button
-          className="detail-edit"
-          label={isEditingSummary ? "완료" : "수정"}
-          onClick={() => {
-            if (isEditingSummary) {
-              setIsEditingSummary(false); // 완료 버튼 클릭 시 수정 완료
-            } else {
-              setIsEditingSummary(true); // 수정 버튼 클릭 시 수정 모드로 전환
-              setTimeout(() => {
-                // setTimeout을 사용하여 렌더링 후 포커스를 설정
-                if (summaryRef.current) {
-                  summaryRef.current.focus();
-                  summaryRef.current.setSelectionRange(
-                    summaryRef.current.value.length,
-                    summaryRef.current.value.length
-                  );
-                }
-              }, 0);
-            }
-          }}
-        />
+
+        {isEditing.summary ? (
+          <div className="detail-edit-buttons">
+            <Button
+              className="detail-cancel"
+              label="취소"
+              onClick={() => handleCancel("summary")}
+            />
+            <Button
+              className="detail-save"
+              label="완료"
+              onClick={() => handleSave("summary")}
+            />
+          </div>
+        ) : (
+          <Button
+            className="detail-edit"
+            label="수정"
+            onClick={() => handleEdit("summary")}
+          />
+        )}
       </div>
 
       {/* 메모 */}
       <div className="memo-container" style={{ height: memoHeight }}>
         <p>메모</p>
         <div className="detail-text-container">
-          {isEditingMemo ? (
+          {isEditing.memo ? (
             <textarea
-              ref={memoRef} // memoRef 연결
+              ref={memoRef}
               className="detail-text-input"
-              value={memo}
-              onChange={(e) => setMemo(e.target.value)}
+              value={values.memo}
+              onChange={(e) => handleChange(e, "memo")}
               onInput={(e) => {
                 const target = e.target;
                 target.style.height = "6rem"; // 높이를 초기화
                 target.style.height = `${target.scrollHeight}px`; // 내용 기반으로 높이 조정
               }}
-              onBlur={() => setIsEditingMemo(false)} // 포커스 해제 시 저장
+              onBlur={() => handleCancel("memo")} // 포커스 해제 시 저장
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   e.preventDefault(); // 엔터키 기본 동작 방지
-                  setIsEditingMemo(false); // 수정 완료 처리
+                  handleSave("memo"); // 수정 완료 처리
                 }
               }}
             />
           ) : (
-            <p className="detail-text">{memo}</p>
+            <p className="detail-text">{values.memo}</p>
           )}
         </div>
-        <Button
-          className="detail-edit"
-          label={isEditingMemo ? "완료" : "수정"}
-          onClick={() => {
-            if (isEditingMemo) {
-              setIsEditingMemo(false); // 완료 버튼 클릭 시 수정 완료
-            } else {
-              setIsEditingMemo(true); // 수정 버튼 클릭 시 수정 모드로 전환
-              setTimeout(() => {
-                // setTimeout을 사용하여 렌더링 후 포커스를 설정
-                if (memoRef.current) {
-                  memoRef.current.focus();
-                  memoRef.current.setSelectionRange(
-                    memoRef.current.value.length,
-                    memoRef.current.value.length
-                  );
-                }
-              }, 0);
-            }
-          }}
-        />
+        {isEditing.memo ? (
+          <div className="detail-edit-buttons">
+            <Button
+              className="detail-cancel"
+              label="취소"
+              onClick={() => handleCancel("memo")}
+            />
+            <Button
+              className="detail-save"
+              label="완료"
+              onClick={() => handleSave("memo")}
+            />
+          </div>
+        ) : (
+          <Button
+            className="detail-edit"
+            label="수정"
+            onClick={() => handleEdit("memo")}
+          />
+        )}
       </div>
 
       <div className="detail-tags">
